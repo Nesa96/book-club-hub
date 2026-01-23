56ab9530b977a4cf438e21e755a0802b2c505b7a
Nesa96 <vanesaarjonilla@gmail.com>
2026-01-23 16:14:11 +0100

    feat: integrate SQLModel with SQLite for persistent book storage

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
-from typing import Optional
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Optional, List, Dict
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Column, JSON
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+# FAST API
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], # Esto permite que React se conecte
@@ -11,13 +19,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# BASE CLASS
 class Review(BaseModel):
     user: str
     rating: float
     comment: str
 
-class Book(BaseModel):
-    id: int
+class Book(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     author: str
     year_read: int
@@ -27,72 +36,67 @@ class Book(BaseModel):
     genre: Optional[str] = "Other"
     pages: Optional[int] = 0
     summary: Optional[str] = None
-    reviews: list[Review] = []
+    reviews: List[Dict] = Field(default=[], sa_column=Column(JSON))
+    media_rating: Optional[int]
 
-# Temporal DB
-books_db = [
-    Book(id=1, title="Aún no estoy muerta", author="Holly Jackson", year=2024, year_read=2025, status="read", 
-         summary="Dentro de siete días, Jet Mason estará muerta. \n " \
-                 "Jet tiene veintisiete años y sigue atrapada en Woodstock, el pueblo de Vermont en el que nació, a la espera de que su vida comience. " \
-                 "\n «Ya lo haré luego», dice siempre. Tiene tiempo. Hasta que, durante la noche de Halloween, Jet sufre un violento ataque por parte de un intruso al que no llega a ver. " \
-                 "\n Sufre una lesión cerebral catastrófica. El médico está seguro de que, al cabo de una semana, sufrirá un aneurisma mortal. " \
-                 "\n Jet nunca había considerado una persona que tuviera enemigos. Pero ahora mira a todo el mundo desde una perspectiva nueva: a su familia, a su ex mejor amiga convertida en cuñada, al que una vez fue su novio. " \
-                 "\n Solo tiene siete días y, mientras su estado no deja de empeorar, Billy, su amigo de la infancia, es la única ayuda con la que puede contar. Aun así, está totalmente dispuesta a terminar algo por una vez en su vida: " \
-                 "\n Jet va a resolver su propio asesinato.",
-         cover_url = "https://m.media-amazon.com/images/I/71KJfkFKxbL.jpg", genre='Thriller', pages=256, reviews= [
-             {
-                 "user": "Vane",
-                 "rating": 4.5,
-                 "comment": "Like it"
-             },
-             {
-                 "user": "Ani",
-                 "rating": 4.5,
-                 "comment": "Like it"
-             },
-             {
-                 "user": "Sarah",
-                 "rating": 4,
-                 "comment": "Like it"
-             }
-         ]),
-    Book(id=2, title="Aún no estoy muerta", author="Holly Jackson", year=2024, year_read=2025, status="read", summary="The history of a death girl solving her murder",
-         cover_url = "https://m.media-amazon.com/images/I/71KJfkFKxbL.jpg", genre='Thriller', pages=256, reviews= [
-             {
-                 "user": "Vane",
-                 "rating": 4.5,
-                 "comment": "Like it"
-             },
-             {
-                 "user": "Ani",
-                 "rating": 4.5,
-                 "comment": "Like it"
-             },
-             {
-                 "user": "Sarah",
-                 "rating": 4,
-                 "comment": "Like it"
-             }
-         ])
-]
+
+#SQLite 
+sqlite_file_name = "books.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 @app.get("/books")
-def get_books(status: Optional[str] = None, year: Optional[int] = None):
+def get_books(status: Optional[str] = None, year: Optional[int] = None, session: Session = Depends(get_session)):
+    statement = select(Book)
     if status and year:
-        return [b for b in books_db if ((b.status == status) and (b.year_read == year))]
+        statement = statement.where((Book.status == status) and (Book.year_read == year))
     elif status:
-        return [b for b in books_db if b.status == status]
+        statement = statement.where(Book.status == status)
     elif year:
-        return [b for b in books_db if b.year_read == year]
-    return books_db
+        statement = statement.where(Book.year_read == year)
+    results = session.exec(statement)
+    return results.all()
 
 @app.get("/stats")
-def get_stats(year: Optional[int] = None):
-    year_books = [b for b in books_db if b.year_read == year]
-    read_books = [b for b in year_books if b.status == "read"]
-    avg_rating = sum(b.rating for b in read_books) / len(read_books) if read_books else 0
+def get_stats(year: Optional[int] = None, session: Session = Depends(get_session)):
+    statement = select(Book).where(Book.status == "read")
+
+    if year:
+        statement = statement.where(Book.year_read == year)
+
+    read_books = session.exec(statement).all()
+    avg_rating = sum(b.media_rating for b in read_books) / len(read_books) if read_books else 0
     return {
         "total_read": len(read_books),
         "average_rating": round(avg_rating, 2),
         "books_per_year": {2025: 2, 2026: 0} # Esto lo automatizaremos
     }
+
+@app.post("/books", response_model=Book)
+def create_book(book: Book, session: Session = Depends(get_session)):
+    session.add(book)
+    session.commit()
+    session.refresh(book)
+    return book
+
+@app.patch("/books/{book_id}", response_model=Book)
+def update_book(book_id: int, update_data: dict, session: Session = Depends(get_session)):
+    db_book = session.get(Book, book_id)
+    if not db_book:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    for key, value in update_data.items():
+        setattr(db_book, key, value)
+
+    session.add(db_book)
+    session.commit()
+    session.refresh(db_book)
+    return db_book
