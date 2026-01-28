7bc7c5e06ac672cc84f63fd1e46a7a50baf3c935
Nesa96 <vanesaarjonilla@gmail.com>
2026-01-28 22:51:44 +0100

    fix: temporal book set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,8 +40,8 @@ function App() {
         // TEMPORAL DATA TO TESTS
         setBooks([
                 { id: 1, title: "Aún no estoy muerta", author: "Holly Jackson", cover_url: "https://m.media-amazon.com/images/I/71KJfkFKxbL.jpg", year_read: 2026, year: 2025, status: "read", genre: "Thriller", pages: 560, summary: "", reviews: [{"user": "Sarah", "rating": 4, "comment": "Like it"}, {"user": "Ani", "rating": 4.5, "comment": "I like it"}, {"user": "Vane", "rating": 4.5, "comment": "I like it"}], media_rating: 4.33},
-                { id: 1, title: "Todo lo que sé sobre el amor", author: "Dolly Alderton", cover_url: "https://books.google.com/books/content?id=yXuhDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api", year_read: 2026, year: 2019, status: "read", genre: "Biografia", pages: 354, summary: "", reviews: [], media_rating: null},
-                { id: 1, title: "La paciente silenciosa", author: "Alex Michaelides", cover_url: "https://books.google.com/books/content?id=LUgz0QEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api", year_read: 2026, year: 2019, status: "recommended", genre: "Thriller", pages: 384, summary: "", reviews: [], media_rating: null}
+                { id: 2, title: "Todo lo que sé sobre el amor", author: "Dolly Alderton", cover_url: "https://books.google.com/books/content?id=yXuhDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api", year_read: 2026, year: 2019, status: "read", genre: "Biografia", pages: 354, summary: "", reviews: [], media_rating: null},
+                { id: 3, title: "La paciente silenciosa", author: "Alex Michaelides", cover_url: "https://books.google.com/books/content?id=LUgz0QEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api", year_read: 2026, year: 2019, status: "recommended", genre: "Thriller", pages: 384, summary: "", reviews: [], media_rating: null}
                 ])
     }, []);
 
